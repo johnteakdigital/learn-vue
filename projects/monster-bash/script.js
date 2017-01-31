bd60947befa471079b9ed83eacd5c12f5fd95051
John e Evanofski <john@teaksf.com>
2017-01-30 21:46:24 -0800

    monster-bash

--- projects/monster-bash/script.js
+++ projects/monster-bash/script.js
@@ -9,9 +9,21 @@ var monsterBash = new Vue({
     playerHeal : 0,
     status : []
   },
+  watch: {
+    playerHp: function() {
+      if (this.playerHp < 1) {
+        this.updateStatus('player', 0, 'death')
+        this.playerHp = 0
+      }
+    },
+    monsterHp: function() {
+      if (this.monsterHp < 1) {
+        this.updateStatus('monster', 0, 'death')
+        this.monsterHp = 0
+      }
+    }
+  },
   computed: {
-    takeDamage: function(char, dmg) {},
-    playerHeals: function(hp) {}
   },
   methods: {
     startNewGame: function() {
@@ -21,30 +33,48 @@ var monsterBash = new Vue({
       this.status = []
     },
     attack: function() {
+      this.monsterAttack(8)
       this.playerDmg = this.computeStatus(5);
       this.updateStatus('player',this.playerDmg, 'attacks');
-      this.monsterAttack(8)
     },
     specialAttack: function() {
-      this.playerDmg = this.computeStatus(8);
-      this.updateStatus('player',this.playerDmg, 'summons fireball and hits');
       this.monsterAttack(10)
+      this.playerDmg = this.computeStatus(8);
+      this.updateStatus('player',this.playerDmg, 'summons fireball');
     },
     heal: function() {
-      this.playerHeal = this.computeStatus(5,1);
-      this.updateStatus('player',this.playerHeal, 'healed');
       this.monsterAttack(5)
+      this.playerHeal = this.computeStatus(10,1);
+      this.updateStatus('player',this.playerHeal, 'healed');
     },
     monsterAttack: function(str) {
       this.monsterDmg = this.computeStatus(str);
       this.updateStatus('monster',this.monsterDmg, 'attacks');
     },
-    updateStatus: function(char, stat, type) {
-      this.status.push({char:char, stat:stat, type:type})
+    updateStatus: function(char, str, type) {
+      if (type != 'death' && this.playerHp > 0 && this.monsterHp > 0) {
+        this.status.unshift({char:char, str:str, type:type})
+        if (char == 'player') {
+          if (type == 'healed') this.playerHp += str
+          else this.monsterHp -= str
+        } else {
+          this.playerHp -= str
+        }
+      } else {
+        var dead = '';
+        if (this.playerHp > 0) dead = 'monster'
+        else dead = 'player'
+        this.status.unshift({char:dead, str:str, type:'death'})
+      }
     },
     computeStatus: function(n, min) {
       var min = min || 0;
       return Math.floor((Math.random() * n) + min)
+    },
+    damageAmount: function(n) {
+      if (n < 25) return 'danger'
+      if (n < 50) return 'warning'
+      else return 'success'
     }
   }
 })
